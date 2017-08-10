85288a4bf9c28d750f6abd0e02bdb15b801f5a22
Barry Allwood <barry.allwood@bjss.com>
2017-08-10 20:24:26 +0100

    Removed useless env vars

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # setup git user
 git config user.name "Node Sauce Connect Cron"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "node-sauce-connect-cron@users.noreply.github.com"
 
 openssl aes-256-cbc -K $encrypted_2610d76ecfd3_key -iv $encrypted_2610d76ecfd3_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
