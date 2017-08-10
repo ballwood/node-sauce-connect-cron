6a0a666db4c08999d49cf07ef1dd68e46abe5373
Barry Allwood <barry.allwood@bjss.com>
2017-08-10 20:23:06 +0100

    Removed broken code Run using npm

--- run.sh
+++ run.sh
@@ -4,15 +4,10 @@
 git config user.name "Node Sauce Connect Cron"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_2610d76ecfd3_key -iv $encrypted_2610d76ecfd3_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
 # run script
-node update-version.js
+npm run update
